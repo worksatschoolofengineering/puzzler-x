8101fa7f9b6d40a5192dbd0bfb6e274d68bcd471
worksatschoolofengineering <100228633+worksatschoolofengineering@users.noreply.github.com>
2023-06-28 06:18:44 +0200

    Update PuzzlerX.java

--- src/main/java/com/puzzlerx/PuzzlerX.java
+++ src/main/java/com/puzzlerx/PuzzlerX.java
@@ -1,36 +1,104 @@
-// Juego de consola de Java Puzzler....
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class PuzzlerX {
-    public static void main(String[] args) {
+public class PuzzleXApp {
+    private JFrame frame;
+    private JPanel puzzlePanel;
+    private JButton startButton;
+    private JButton stopButton;
+    private JTextField movesField;
+    private JTextField timeField;
+    
+    private int movesCount;
+    private Timer timer;
+    private int seconds;
+    
+    public PuzzleXApp() {
         // Crear la ventana principal
-        JFrame frame = new JFrame("Puzzler-X");
+        frame = new JFrame("Puzzle-X");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 500);
-
-        // Crear los componentes de la interfaz
-        JPanel boardPanel = new JPanel(new GridLayout(4, 4));
-        JLabel moveLabel = new JLabel("Move: 0");
-        JLabel timeLabel = new JLabel("Time: 00:00");
-        JButton startButton = new JButton("Start");
-        JButton stopButton = new JButton("Stop");
-
-        // Establecer los colores de fondo y de texto
-        frame.getContentPane().setBackground(Color.WHITE);
-        boardPanel.setBackground(Color.WHITE);
-        moveLabel.setForeground(Color.BLACK);
-        timeLabel.setForeground(Color.BLACK);
-
-        // Agregar los componentes a la ventana
-        frame.add(boardPanel, BorderLayout.CENTER);
-        frame.add(moveLabel, BorderLayout.NORTH);
-        frame.add(timeLabel, BorderLayout.SOUTH);
-        frame.add(startButton, BorderLayout.WEST);
-        frame.add(stopButton, BorderLayout.EAST);
-
+        frame.setLocationRelativeTo(null);
+        
+        // Crear el panel del tablero de juego
+        puzzlePanel = new JPanel();
+        puzzlePanel.setLayout(new GridLayout(4, 4, 5, 5));
+        
+        // Crear los botones del tablero de juego
+        for (int i = 1; i <= 15; i++) {
+            JButton button = new JButton(String.valueOf(i));
+            puzzlePanel.add(button);
+        }
+        
+        // Crear el botón "Start"
+        startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                startGame();
+            }
+        });
+        
+        // Crear el botón "Stop"
+        stopButton = new JButton("Stop");
+        stopButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                stopGame();
+            }
+        });
+        
+        // Crear el contador de movimientos
+        movesField = new JTextField("Moves: 0");
+        movesField.setEditable(false);
+        
+        // Crear el contador de tiempo
+        timeField = new JTextField("Time: 0s");
+        timeField.setEditable(false);
+        
+        // Añadir los componentes a la ventana
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().add(puzzlePanel, BorderLayout.CENTER);
+        frame.getContentPane().add(startButton, BorderLayout.NORTH);
+        frame.getContentPane().add(stopButton, BorderLayout.SOUTH);
+        frame.getContentPane().add(movesField, BorderLayout.WEST);
+        frame.getContentPane().add(timeField, BorderLayout.EAST);
+        
         // Mostrar la ventana
         frame.setVisible(true);
     }
+    
+    private void startGame() {
+        // Iniciar el juego
+        movesCount = 0;
+        seconds = 0;
+        
+        // Actualizar los contadores
+        movesField.setText("Moves: " + movesCount);
+        timeField.setText("Time: " + seconds + "s");
+        
+        // Iniciar el temporizador
+        timer = new Timer(1000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                seconds++;
+                timeField.setText("Time: " + seconds + "s");
+            }
+        });
+        timer.start();
+    }
+    
+    private void stopGame() {
+        // Detener el juego
+        if (timer != null) {
+            timer.stop();
+        }
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new PuzzleXApp();
+            }
+        });
+    }
 }
+
